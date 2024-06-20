6def0c6d365a1ea0ef3d27389f19df0686f98aec
farizahh <90184606+farizahh@users.noreply.github.com>
2024-06-20 22:14:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -102,10 +102,7 @@ if option == 'IMDB Top Movies':
     st.write("Berikut merupakan hasil dari scraping yang diambil dari website IMDB, hasil dari scraping tersebut mengambil elemen judul film, rating film, tahun tayang film, durasi film, dan kualifikasi umur penonton film.")
     st.dataframe(df_imdb)
 
-    # # Buat visualisasi untuk 10 baris teratas
-    # st.subheader("Visualisasi Top 10 Rating Film di IMDB")
-    # 
-
+    st.subheader("Hasil visualisasi data IMDB")
     # 1. Comparison
     st.subheader('1. Comparison (perbandingan)')
     st.write("Visualisasi di bawah ini mengambil data judul film dan ratingnya yang menunjukkan hasil dari rating film yang diambil dipilih hanya 10 kolom teratas. Dapat di lihat bagaimana rating film bervariasi dari tahun ke tahun.")
